ba69b0d90f1bb0928b6b8a5b2103f407415327bc
Pellegrino Smiling <n.pellegrino@smiling.video>
2023-09-27 16:03:33 +0200

    add BlogLayout and logic

--- src/components/Home/FeaturedPosts.js
+++ src/components/Home/FeaturedPosts.js
@@ -1,5 +1,28 @@
+import { sortBlogs } from "@/src/utils";
 import React from "react";
+import { BlogLayoutOne } from "../Blog/BlogLayoutOne";
+import { BlogLayoutTwo } from "../Blog/BlogLayoutTwo";
 
-export const FeaturedPosts = () => {
-    return <div>FeaturedPosts</div>;
+export const FeaturedPosts = ({ blogs }) => {
+    const sortedblogs = sortBlogs(blogs);
+
+    return (
+        <section className="w-full mt-32 px-32 flex flex-col items-center justify-center">
+            <h2 className="w-full inline-block font-bold capitalize text-4xl">
+                Featured Posts
+            </h2>
+
+            <div className="grid grid-cols-2 grid-rows-2 gap-6 mt-16">
+                <article className="col-span-1 row-span-2 relative">
+                    <BlogLayoutOne blog={sortedblogs[2]} />
+                </article>
+                <article className="col-span-1 row-span-1 relative">
+                    <BlogLayoutTwo blog={sortedblogs[3]} />
+                </article>
+                <article className="col-span-1 row-span-1 relative">
+                    <BlogLayoutTwo blog={sortedblogs[4]} />
+                </article>
+            </div>
+        </section>
+    );
 };
