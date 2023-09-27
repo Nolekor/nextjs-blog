6898a22d47ecaa1b393b3c1c6b26c2f9790eaadf
Pellegrino Smiling <n.pellegrino@smiling.video>
2023-09-27 13:01:17 +0200

    add HomeCoverSection component and logic

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import { allBlogs } from "contentlayer/generated";
 import { HomeCoverSection } from "../components/Home/HomeCoverSection";
+import { FeaturedPosts } from "../components/Home/FeaturedPosts";
 
 export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <HomeCoverSection blogs={allBlogs} />
+            <FeaturedPosts blogs={allBlogs} />
         </main>
     );
 }
---
+++ src/components/Home/FeaturedPosts.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const FeaturedPosts = () => {
+    return <div>FeaturedPosts</div>;
+};
