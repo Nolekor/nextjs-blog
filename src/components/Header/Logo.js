826582c89e72fb2ce9d96beb4349e41879445105
Pellegrino Smiling <n.pellegrino@smiling.video>
2023-10-04 17:59:42 +0200

    Mobile stuff

--- src/components/Header/Logo.js
+++ src/components/Header/Logo.js
@@ -5,14 +5,14 @@ import profileImg from "@/public/profile-img.jpg";
 export const Logo = () => {
     return (
         <Link href="/" className="flex items-center text-dark dark:text-light">
-            <div className="w-16 rounded-full overflow-hidden border border-solid border-dark dark:border-light mr-4">
+            <div className="w-12 md:w-16 rounded-full overflow-hidden border border-solid border-dark dark:border-light mr-2 md:mr-4">
                 <Image
                     src={profileImg}
                     alt="Nicola Pellegrino"
                     className="w-full h-auto rounded-full"
                 />
             </div>
-            <span className="text-xl font-bold dark:font-semibold">
+            <span className="text-lg md:text-xl font-bold dark:font-semibold">
                 Nolekor
             </span>
         </Link>
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { Logo } from "./Logo";
 import Link from "next/link";
 import { GithubIcon, LinkedinIcon, SunIcon } from "../Icons";
@@ -8,11 +8,48 @@ import { useThemeSwitch } from "../Hooks/useThemeSwitch";
 
 const Header = () => {
     const [mode, setMode] = useThemeSwitch();
-
+    const [click, setClick] = useState(false);
+    const toggle = () => {
+        setClick(!click);
+    };
     return (
-        <header className="w-full flex p-4 px-10 items-center justify-between">
+        <header className="w-full flex p-4 px-5 sm:px-10 items-center justify-between">
             <Logo />
-            <nav className="w-max py-3 px-8 border border-solid border-dark rounded-full font-medium capitalize flex items-center fixed top-6 right-1/2 translate-x-1/2 bg-light/80 backdrop-blur-sm z-50">
+            <button className="inline-block sm:hidden z-50" onClick={toggle}>
+                <div className="w-6 cursor-pointer transition-all ease duration-300">
+                    <div className="relative">
+                        <span
+                            className="absolute top-0 inline-block w-full h-0.5 bg-dark dark:bg-light roudend transition-all ease duration-200"
+                            style={{
+                                transform: click
+                                    ? "rotate(-45deg) translateY(0)"
+                                    : "rotate(0deg) translateY(6px)",
+                            }}
+                        >
+                            &nbsp;
+                        </span>
+                        <span
+                            className="absolute top-0 inline-block w-full h-0.5 bg-dark dark:bg-light roudend transition-all ease duration-200"
+                            style={{
+                                opacity: click ? 0 : 1,
+                            }}
+                        >
+                            &nbsp;
+                        </span>
+                        <span
+                            className="absolute top-0 inline-block w-full h-0.5 bg-dark dark:bg-light roudend transition-all ease duration-200"
+                            style={{
+                                transform: click
+                                    ? "rotate(45deg) translateY(0)"
+                                    : "rotate(0deg) translateY(-6px)",
+                            }}
+                        >
+                            &nbsp;
+                        </span>
+                    </div>
+                </div>
+            </button>
+            <nav className="w-max py-3 px-8 border border-solid border-dark rounded-full font-medium capitalize hidden sm:flex items-center fixed top-6 right-1/2 translate-x-1/2 bg-light/80 backdrop-blur-sm z-50">
                 <Link href="/" className="mr-2">
                     Home
                 </Link>
@@ -28,7 +65,7 @@ const Header = () => {
                     <SunIcon />
                 </button>
             </nav>
-            <div>
+            <div className="hidden sm:flex items center">
                 <a
                     href={siteMetadata.linkedin}
                     className="inline-block w-6 h-6 mr-4"
@@ -39,7 +76,7 @@ const Header = () => {
                     href={siteMetadata.github}
                     className="inline-block w-6 h-6 mr-4"
                 >
-                    <GithubIcon className="hover:scale-125 transition-all ease duration-200" />
+                    <GithubIcon className="hover:scale-125 transition-all ease duration-200 dark:fill-light" />
                 </a>
             </div>
         </header>
