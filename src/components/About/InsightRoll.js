d06554b917e0ce6c3bc22c7e04508d1a454fb5cc
Pellegrino Smiling <n.pellegrino@smiling.video>
2023-10-04 11:39:43 +0200

    About and contanct page creation

---
+++ src/components/About/InsightRoll.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function InsightRoll({ insights }) {
+    return (
+        <div className="w-full bg-accent text-light whitespace-nowrap overflow-hidden">
+            <div className="animate-roll w-full py-3 flex items-center justify-center capitalize font-semibold tracking-wider text-base">
+                {insights.map((text) => (
+                    <div key={text}>
+                        {text}
+                        <span className="px-4">|</span>{" "}
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
